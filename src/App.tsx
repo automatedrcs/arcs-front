0c8a3df6ee035c5d6bd3c1e0c8590c6390db9676
Junah Park <park.junah95@gmail.com>
2023-09-27 16:53:27 -0700

    remove react strict mode>

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 
 import './App.css'
+import TestConnectionButton from './components/TestConnectionButton';
+
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <React.StrictMode>
+    <div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -18,7 +21,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </React.StrictMode>
+      <TestConnectionButton />
+    </div>
   )
 }
 
