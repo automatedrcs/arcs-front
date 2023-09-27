6676fcbadb53e88a8d6111a8efb18819c910cb13
Junah Park <park.junah95@gmail.com>
2023-09-26 21:33:25 -0700

    vite_api_url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,11 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   renderers: ['@astrojs/renderer-react'],
-  integrations: [react()]
+  integrations: [react()],
+  vite: {
+    // Any Vite-specific configuration goes here
+    define: {
+      'process.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL)
+    }
+  }
 });
