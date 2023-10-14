fc8b13d1f0c9ee72ab49ef33a7fea7ee2cd36d57
Junah Park <park.junah95@gmail.com>
2023-10-13 23:10:50 -0700

    signup refactor

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -1,8 +1,7 @@
-// components/Signup.tsx
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { apiUrl } from '../config';
-import "./Signup.css";
+import './Signup.css';
 
 const Signup: React.FC = () => {
     const [username, setUsername] = useState<string>('');
@@ -10,21 +9,25 @@ const Signup: React.FC = () => {
     const [password, setPassword] = useState<string>('');
     const [error, setError] = useState<string>('');
     const [loading, setLoading] = useState<boolean>(false);
-    const navigate = useNavigate();
     const [orgEmail, setOrgEmail] = useState<string>('');
+    const [signupSuccess, setSignupSuccess] = useState<boolean>(false);
+    const navigate = useNavigate();
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         setLoading(true);
-        
+
         try {
             const response = await fetch(apiUrl + '/user/signup', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ username, email, password, organization_email: orgEmail})
-            });           
+                body: JSON.stringify({ username, email, password, organization_email: orgEmail })
+            });
             if (response.ok) {
-                navigate('/'); // Redirect to entry page after signup
+                setSignupSuccess(true);
+                setTimeout(() => {
+                    navigate('/login');
+                }, 3000);
             } else {
                 const data = await response.json();
                 setError(data.error || 'Failed to sign up');
@@ -39,54 +42,57 @@ const Signup: React.FC = () => {
     return (
         <div className="signup">
             <h2>Sign Up</h2>
+            {signupSuccess && <p className="success">Signup successful. Redirecting to login...</p>}
             {error && <p className="error">{error}</p>}
             {loading && <p>Loading...</p>}
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>Username:</label>
-                    <input
-                        type="text"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                        required
-                    />
-                </div>
-                <div>
-                    <label>Email:</label>
-                    <input
-                        type="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        required
-                    />
-                </div>
-                <div>
-                    <label>Password:</label>
-                    <input
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                    />
-                </div>
-                <div>
-                    <label>Organization Email:</label>
-                    <input
-                        type="email"
-                        value={orgEmail}
-                        onChange={(e) => setOrgEmail(e.target.value)}
-                        required
-                    />
-                </div>
-                <div>
-                    <button type="submit" disabled={loading}>Sign Up</button>
-                </div>
-            </form>
+            {!signupSuccess && (
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <label>Username:</label>
+                        <input
+                            type="text"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div>
+                        <label>Email:</label>
+                        <input
+                            type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div>
+                        <label>Password:</label>
+                        <input
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div>
+                        <label>Organization Email:</label>
+                        <input
+                            type="email"
+                            value={orgEmail}
+                            onChange={(e) => setOrgEmail(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div>
+                        <button type="submit" disabled={loading}>Sign Up</button>
+                    </div>
+                </form>
+            )}
             <div>
                 <a href="/login">Already have an account? Log in</a>
             </div>
         </div>
-    );    
+    );
 };
 
 export default Signup;
