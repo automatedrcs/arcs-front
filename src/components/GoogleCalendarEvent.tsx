2a756b9b226d2bd7ce089352fa5e6369fc79b64f
Junah Park <park.junah95@gmail.com>
2023-10-21 22:59:53 -0700

    woewirhj

--- src/components/GoogleCalendarEvent.tsx
+++ src/components/GoogleCalendarEvent.tsx
@@ -1,11 +1,10 @@
-// components/GoogleCalendarEvent.tsx
 import React from 'react';
 import { GoogleCalendarEventData } from '../types/GoogleTypes';
 
 const GoogleCalendarEvent: React.FC<GoogleCalendarEventData> = ({ id, start, end, summary }) => {
     const eventStartTime = new Date(start.dateTime);
     const eventEndTime = new Date(end.dateTime);
-    
+
     const formatTime = (date: Date) => {
         return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
     };
@@ -18,14 +17,7 @@ const GoogleCalendarEvent: React.FC<GoogleCalendarEventData> = ({ id, start, end
 
     return (
         <div key={id} className="event">
-            <div className="event-title" style={{
-                overflow: 'hidden',
-                textOverflow: 'ellipsis',
-                display: '-webkit-box',
-                WebkitLineClamp: 2,
-                WebkitBoxOrient: 'vertical',
-                maxHeight: '2.4em' // Assuming line-height of 1.2em, adjust as needed
-            }}>{truncatedSummary}</div>
+            <div className="event-title">{truncatedSummary}</div>
             <div className="event-time-window">{timeWindow}</div>
         </div>
     );
