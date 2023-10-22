a9cb682adecb799465e7e8839a043b1226e0fe2a
Junah Park <park.junah95@gmail.com>
2023-10-21 21:45:20 -0700

    woeirhjoweuihro

--- src/components/GoogleCalendarEvent.tsx
+++ src/components/GoogleCalendarEvent.tsx
@@ -5,22 +5,24 @@ import { GoogleCalendarEventData } from '../types/GoogleTypes';
 const GoogleCalendarEvent: React.FC<GoogleCalendarEventData> = ({ id, start, end, summary }) => {
     const eventStartTime = new Date(start.dateTime);
     const eventEndTime = new Date(end.dateTime);
-    const eventStartMinutes = eventStartTime.getHours() * 60 + eventStartTime.getMinutes();
-    const eventEndMinutes = eventEndTime.getHours() * 60 + eventEndTime.getMinutes();
-    const minutesInADay = 24 * 60;
     const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    
+    const formatTime = (date: Date) => {
+        return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+    };
+
+    const timeWindow = `${formatTime(eventStartTime)} - ${formatTime(eventEndTime)}`;
 
     return (
         <div
             key={id}
             className="event"
             style={{
-                top: `${(eventStartMinutes / minutesInADay) * 100}%`,
-                height: `${((eventEndMinutes - eventStartMinutes) / minutesInADay) * 100}%`,
                 gridColumn: `${daysOfWeek.indexOf(eventStartTime.toLocaleDateString('en-US', { weekday: 'short' })) + 1}`
             }}
         >
-            {summary}
+            <div className="event-title">{summary}</div>
+            <div className="event-time-window">{timeWindow}</div>
         </div>
     );
 };
