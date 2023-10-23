55db79e28254506fc793b1b5c939530968c116f3
Junah Park <park.junah95@gmail.com>
2023-10-22 20:19:56 -0700

    oweirjwoeirj

--- src/components/GoogleCalendarEvent.tsx
+++ src/components/GoogleCalendarEvent.tsx
@@ -1,23 +1,31 @@
-// components/GoogleCalendarEvent.tsx
 import React from 'react';
 import { GoogleCalendarEventProps } from '../types/GoogleTypes';
 
 const GoogleCalendarEvent: React.FC<GoogleCalendarEventProps> = ({ id, start, end, summary, style }) => {
+    let eventStartTime = null;
+    let eventEndTime = null;
 
-    const eventStartTime = new Date(start.dateTime);
-    const eventEndTime = new Date(end.dateTime);
+    if (start.dateTime) {
+        eventStartTime = new Date(start.dateTime);
+        eventEndTime = new Date(end.dateTime);
+    }
 
     const formatTime = (date: Date) => {
-        const timeString = date.toLocaleTimeString([], { hour12: true, hour: 'numeric', minute: '2-digit' });
-        return timeString.toLowerCase();
+        if (!isNaN(date.getTime())) {
+            return date.toLocaleTimeString([], { hour12: true, hour: 'numeric', minute: '2-digit' }).toLowerCase();
+        } else {
+            return '';
+        }
     };
-    
-    const timeWindow = `${formatTime(eventStartTime)} - ${formatTime(eventEndTime)}`;
+
+    const timeWindow = eventStartTime && eventEndTime
+        ? `${formatTime(eventStartTime)} - ${formatTime(eventEndTime)}`
+        : '';
 
     return (
         <div key={id} className="event" style={style}>
             <div className="event-title">{summary}</div>
-            <div className="event-time-window">{timeWindow}</div>
+            {timeWindow && <div className="event-time-window">{timeWindow}</div>}
         </div>
     );
 };
