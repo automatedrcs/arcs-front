93249f92cfc3293950f976a6b8cfbebf203eb641
Junah Park <park.junah95@gmail.com>
2023-10-22 20:43:57 -0700

    woeijroweijr

--- src/components/GoogleCalendarWeekly.tsx
+++ src/components/GoogleCalendarWeekly.tsx
@@ -19,59 +19,23 @@ const GoogleCalendarWeekly: React.FC<GoogleCalendarWeeklyProps> = ({ events, wee
     const endDate = new Date(weekStartDate.getTime() + 6 * 24 * 60 * 60 * 1000);
 
     const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-
-    // Create a dictionary to hold events for each day
-    const eventsByDay: { [key: string]: GoogleCalendarEventData[] } = {};
-
-    // Group events by day
-    events.forEach((event) => {
-        // Check if event.start.dateTime is present
-        if (event.start.dateTime) {
-            const day = new Date(event.start.dateTime).toDateString();
-            if (!eventsByDay[day]) {
-                eventsByDay[day] = [];
-            }
-            eventsByDay[day].push(event);
-        } else if (event.start.date) {
-            const day = new Date(event.start.date).toDateString();
-            if (!eventsByDay[day]) {
-                eventsByDay[day] = [];
-            }
-            eventsByDay[day].push(event);
-        } else {
-            console.log('no date nor dateTime for event');
-        }
-    });
-
-    // Create a mapping between the index and the full date string
-    const indexToDateMapping: { [key: number]: string } = {};
-    daysOfWeek.forEach((_, index) => {
-        const currentDate = new Date(weekStartDate);
-        currentDate.setDate(currentDate.getDate() + index);
-        indexToDateMapping[index] = currentDate.toDateString();
-    });
+    const sortedEvents = [...events].sort((a, b) => new Date(a.start.dateTime).getTime() - new Date(b.start.dateTime).getTime());
 
     return (
         <div className="google-calendar-weekly">
             <div className="week-navigation">
-                <button onClick={handlePreviousWeek}>&larr; Prev</button>
+                <button onClick={handlePreviousWeek}>&larr; Prev</button> {/* Change text for better spacing */}
                 <span>{weekStartDate.toLocaleDateString()} - {endDate.toLocaleDateString()}</span>
-                <button onClick={handleNextWeek}>Next &rarr;</button>
+                <button onClick={handleNextWeek}>Next &rarr;</button> {/* Change text for better spacing */}
             </div>
             <div className="grid-container">
-                {daysOfWeek.map((day, columnIndex) => (
-                    <div key={columnIndex} className="grid-item-day" style={{ gridColumn: columnIndex + 1 }}>
+                {daysOfWeek.map((day, index) => (
+                    <div key={index} className="grid-item-day" style={{ gridColumn: index + 1 }}>
                         {day}
-                        
-                        {/* Render events for the current day */}
-                        {eventsByDay[indexToDateMapping[columnIndex]]?.map((event: GoogleCalendarEventData, eventIndex) => (
-                            <EventComponent
-                                key={event.id}
-                                {...event}
-                                style={{ gridColumn: columnIndex + 1, gridRow: eventIndex + 2 }}
-                            />
-                        ))}
                     </div>
+                ))}
+                {sortedEvents.map((event: GoogleCalendarEventData) => (
+                    <EventComponent key={event.id} {...event} />
                 ))}
             </div>
         </div>
