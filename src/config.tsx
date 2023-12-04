5bc9f6479444a8b0a341aa9fa21bfcfdfa38e7a5
Jainam <jainam_s@icloud.com>
2023-12-04 10:25:34 -0800

    updated config

--- src/config.tsx
+++ src/config.tsx
@@ -1,7 +1,9 @@
 // config.tsx
 
-export const apiUrl = "https://arcs-back-service-ctl3t7ldeq-uc.a.run.app"
+export const apiUrl = "https://arcs-api-ctl3t7ldeq-uc.a.run.app"
 
-// https://arcs-back-service-ctl3t7ldeq-uc.a.run.app
+// https://arcs-back-service-ctl3t7ldeq-uc.a.run.app - Python
 
-// https://arcs-api-ctl3t7ldeq-uc.a.run.app
+// https://arcs-api-ctl3t7ldeq-uc.a.run.app - Nest
+
+// http://localhost:8080 - local, assuming you have the backend service running
